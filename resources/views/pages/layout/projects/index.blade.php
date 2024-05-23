a63cc2f6242068af251fe18e7cc6e36dce494915
bmabillard <brice.mabillard@pcprofi.ch>
2024-05-23 17:17:51 +0200

    enum state

--- resources/views/pages/layout/projects/index.blade.php
+++ resources/views/pages/layout/projects/index.blade.php
@@ -41,8 +41,10 @@
                     <td class="px-6 py-4 text-center">
                         {{$project->price}}.-
                     </td>
-                    <td class="px-6 py-4 text-center">
-                        {{\App\ProjectStateEnum::getLabel($project->state)}}
+                    <td class="py-4 text-center">
+                        <span class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">
+                            {{\App\ProjectStateEnum::getLabel($project->state)}}
+                        </span>
                     </td>
                     <td class="px-6 py-4">
                         @if($project->url_prod)
